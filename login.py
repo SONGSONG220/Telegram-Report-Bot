766a26ca1835f17d189c10dbc9f42b63480cfa3e
Stark <96779742+urstark@users.noreply.github.com>
2026-02-04 18:44:51 +0530

    𝑆𝑡𝑎𝑟𝑘

--- login.py
+++ login.py
@@ -1,16 +1,19 @@
 import sys
 import re
+import json
 from pyrogram import Client, errors
 
 
 def main(Target, String):
 
     # workdir = 'session/'
+    # pyrogram.errors.exceptions.bad_request_400.UsernameNotOccupied
     SessionString = String
     with Client(name="Session", session_string=SessionString) as app:
         User = app.get_me()
         if app.get_me():
-            print(User)
+            user_info = {"id": User.id, "first_name": User.first_name}
+            print(json.dumps(user_info))
             try:
                 app.join_chat(Target)
             except errors.UserAlreadyParticipant:
@@ -18,7 +21,7 @@ def main(Target, String):
             except:
                 pass
         else:
-            print("Loggined Failed !")
+            sys.exit("Loggined Failed !")
 
 
 if __name__ == "__main__":
