809db484ce17d50ca521783f87469ff60dfc5bb7
Song <satypaldhakad0@gmail.com>
2026-03-23 09:03:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,7 +9,8 @@ class Config(object):
     
     # Other Configs
     BOT_START_TIME = time.time()
-    OWNER = int(os.environ.get("OWNER", "1118345061")) # ⚠️ Required
+    OWNER = int(os.environ.get OWNER = [7812646657, 8639592397]
+ # ⚠️ Required
 
     # Web Response Config
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
